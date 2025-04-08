54683c2072241139607f714d3851acce66a19223
Houssem Saiah <houcemesaiah@gmail.com>
2025-04-08 18:43:43 -0400

    Ajout de l'en-tête auteur dans GestionnaireReservations.cs

--- Model/GestionnaireReservations.cs
+++ Model/GestionnaireReservations.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿//***************************************************************************************
+//  + Nom du fichier: GestionnaireReservations.cs
+//  + Nom de la classe: GestionnaireReservations
+//  + Description du rôle du fichier: Cette classe contient les réservations et permet
+//    de calculer certaines statistiques nécessaires pour la gestion du cinéma.
+//  + Auteur: Houssem Saiah
+//  + Créer le: 2025-04-08
+//***************************************************************************************
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;

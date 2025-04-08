954368a867bca05874c3625dae899d480096e3e6
Houssem Saiah <houcemesaiah@gmail.com>
2025-04-08 18:35:12 -0400

    Ajout de l'en-tête auteur dans Reservation.cs

--- Model/Reservation.cs
+++ Model/Reservation.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿//***************************************************************************************
+//  + Nom du fichier: Reservation.cs
+//  + Nom de la classe: Reservation
+//  + Description du rôle du fichier: Cette classe représente les reservations des clients à des films.
+//  + Auteur: Houssem Saiah
+//  + Créer le: 2025-04-08
+//***************************************************************************************
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;

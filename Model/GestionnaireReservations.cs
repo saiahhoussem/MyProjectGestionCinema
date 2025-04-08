f117f9e930155e4865ff449f74cccda40dbbd966
Houssem Saiah <houcemesaiah@gmail.com>
2025-04-08 13:17:54 -0400

    Etablir la strusture du projet.

---
+++ Model/GestionnaireReservations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProjectGestionCinema.Model
+{
+    internal class GestionnaireReservations
+    {
+    }
+}

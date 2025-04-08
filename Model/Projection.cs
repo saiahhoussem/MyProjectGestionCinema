7902d6fa09556dad1b644f1ac173f8f19bdac0f0
Houssem Saiah <houcemesaiah@gmail.com>
2025-04-08 14:40:24 -0400

    Ajout de l'en-tête auteur dans Projection.cs

--- Model/Projection.cs
+++ Model/Projection.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿//***************************************************************************************
+//  + Nom du fichier: Projection.cs
+//  + Nom de la classe: Projection
+//  + Description du rôle du fichier: Cette classe représente les projections des films.
+//  + Auteur: Houssem Saiah
+//  + Créer le: 2025-04-08
+//***************************************************************************************
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;

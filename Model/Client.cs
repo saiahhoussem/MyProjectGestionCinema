07ed40e0b1407a49240025e2a2ca7cbff64ee993
Houssem Saiah <houcemesaiah@gmail.com>
2025-04-08 14:43:13 -0400

    Ajout de l'en-tête auteur dans Client.cs

--- Model/Client.cs
+++ Model/Client.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿//***************************************************************************************
+//  + Nom du fichier: Client.cs
+//  + Nom de la classe: Client
+//  + Description du rôle du fichier: Cette classe représente les abonnés au cinéma.
+//  + Auteur: Houssem Saiah
+//  + Créer le: 2025-04-08
+//***************************************************************************************
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;

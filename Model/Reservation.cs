7d496b6947013323813978da92a2a7b09a93ba9d
Houssem Saiah <houcemesaiah@gmail.com>
2025-04-11 22:57:57 -0400

    Ajout du constructeur de la classe Reservation.

--- Model/Reservation.cs
+++ Model/Reservation.cs
@@ -21,5 +21,37 @@ namespace MyProjectGestionCinema.Model
         private Projection m_laProjectionAssociee;
         private int m_iNbrPlaces;
 
+        /// <summary>
+        /// Initialise une nouvelle instance de la classe <see cref="Reservation"/> avec les informations du client, 
+        /// de la projection et du nombre de places souhaitées.
+        /// </summary>
+        /// <param name="leClient">Le client qui effectue la réservation.</param>
+        /// <param name="laProjection">La projection à laquelle le client souhaite assister.</param>
+        /// <param name="nbrPlaces">Le nombre de places à réserver (entre 1 et 10).</param>
+        /// <exception cref="ArgumentNullException">
+        /// Levée si le client ou la projection est null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Levée si le nombre de places est inférieur à 1 ou supérieur à 10.
+        /// </exception
+        public Reservation( Client leClient, Projection laProjection, int nbrPlaces ) 
+        { 
+            if (leClient == null)
+            {
+                throw new ArgumentNullException("Une réservation doit être associée à un client non null.");
+            }
+            m_leClient = leClient;
+
+            if (laProjection == null)
+            {
+                throw new ArgumentNullException("Une projection doit être associée à une projection non null.");
+            }
+            m_laProjectionAssociee = laProjection;
+
+            //à modifier
+            m_iNbrPlaces = nbrPlaces;
+
+        }
+
     }
 }

cc42dce6c3f5adbe8f54993cc6a29165c23ee102
Houssem Saiah <houcemesaiah@gmail.com>
2025-04-12 18:51:41 -0400

    Ajout de la structure StatiqtiquesSalles.

---
+++ Model/StatistiquesSalle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProjectGestionCinema.Model
+{
+    public struct StatistiquesSalle
+    {
+        public string NomSalle { get; set; }
+        public decimal MontantMensuel { get; set; }
+
+        public StatistiquesSalle(string nomSalle, decimal montantMensuel)
+        {
+            NomSalle = nomSalle;
+            MontantMensuel = montantMensuel;
+        }
+
+        public override string ToString()
+        {
+            return $"{NomSalle} : {MontantMensuel}$";
+        }
+    }
+
+}
